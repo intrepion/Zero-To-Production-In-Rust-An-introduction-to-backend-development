27f89c51d468165dc1682c4877b8850505bed4d4
Oliver Forral <intrepion@gmail.com>
2022-01-11 15:48:17 -0700

    renamed run local to check local

--- scripts/check_local.sh
+++ scripts/check_local.sh
@@ -1,7 +1,8 @@
 cargo check \
-&& cargo fmt -- --check \
 && cargo audit --ignore RUSTSEC-2021-0131 --ignore RUSTSEC-2020-0159 --ignore RUSTSEC-2020-0071 \
 && cargo clippy -- -D warnings \
-&& cargo +nightly udeps \
 && cargo test --verbose \
+&& cargo fmt -- --check \
+&& cargo +nightly udeps \
+&& cargo sqlx prepare --check -- --bin zero2prod
 && RUST_LOG=trace cargo run
