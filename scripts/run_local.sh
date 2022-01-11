698ad1e3331e06c2dad69164006ed8bade0ba902
Oliver Forral <intrepion@gmail.com>
2022-01-10 20:05:24 -0700

    cleaning up unused dependencies

--- scripts/run_local.sh
+++ scripts/run_local.sh
@@ -2,5 +2,6 @@ cargo check \
 && cargo fmt -- --check \
 && cargo audit --ignore RUSTSEC-2021-0131 --ignore RUSTSEC-2020-0159 --ignore RUSTSEC-2020-0071 \
 && cargo clippy -- -D warnings \
+&& cargo +nightly udeps \
 && cargo test --verbose \
 && RUST_LOG=trace cargo run
