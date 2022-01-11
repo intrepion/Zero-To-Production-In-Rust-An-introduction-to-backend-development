2f3970437ae8b505757d9cbfc0376b950ef83e42
Oliver Forral <intrepion@gmail.com>
2022-01-10 19:42:33 -0700

    adding tracing messages to health check endpoint

--- zero2prod/src/routes/health_check.rs
+++ zero2prod/src/routes/health_check.rs
@@ -1,5 +1,10 @@
 use actix_web::HttpResponse;
 
 pub async fn health_check() -> HttpResponse {
+    tracing::trace!("Insert trace message here.");
+    tracing::debug!("Insert debug message here.");
+    tracing::info!("Insert info message here.");
+    tracing::warn!("Insert warn message here.");
+    tracing::error!("Insert error message here.");
     HttpResponse::Ok().finish()
 }
