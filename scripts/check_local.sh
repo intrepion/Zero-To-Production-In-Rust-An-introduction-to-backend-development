e247a01e9f8e2b399116cf53046b96ebbe29b173
Oliver Forral <intrepion@gmail.com>
2022-01-11 19:26:05 -0700

    separating the configuration into different places based on environments

--- scripts/check_local.sh
+++ scripts/check_local.sh
@@ -1,8 +1,16 @@
-cargo check \
-&& cargo audit --ignore RUSTSEC-2021-0131 --ignore RUSTSEC-2020-0159 --ignore RUSTSEC-2020-0071 \
-&& cargo clippy -- -D warnings \
-&& cargo test --verbose \
-&& cargo fmt -- --check \
-&& cargo +nightly udeps \
-&& cargo sqlx prepare --check -- --bin zero2prod \
-&& RUST_LOG=trace cargo run
+echo "---- cargo check ----" && \
+cargo check && \
+echo "---- cargo audit ----" && \
+cargo audit --ignore RUSTSEC-2021-0131 --ignore RUSTSEC-2020-0159 --ignore RUSTSEC-2020-0071 && \
+echo "---- cargo clippy ----" && \
+cargo clippy -- -D warnings && \
+echo "---- cargo test ----" && \
+cargo test --verbose && \
+echo "---- cargo fmt ----" && \
+cargo fmt -- --check && \
+echo "---- cargo udeps ----" && \
+cargo +nightly udeps && \
+echo "---- cargo sqlx ----" && \
+cargo sqlx prepare --check -- --bin zero2prod && \
+echo "---- cargo run ----" && \
+RUST_LOG=trace cargo run
