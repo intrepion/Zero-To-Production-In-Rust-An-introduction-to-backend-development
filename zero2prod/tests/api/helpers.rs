48ae687047b9e80ff73440ecb8bfc48ed74aadac
Oliver Forral <intrepion@gmail.com>
2022-01-13 14:55:30 -0700

    making sure subscribe sends an email

--- zero2prod/tests/api/helpers.rs
+++ zero2prod/tests/api/helpers.rs
@@ -1,6 +1,7 @@
 use once_cell::sync::Lazy;
 use sqlx::{Connection, Executor, PgConnection, PgPool};
 use uuid::Uuid;
+use wiremock::MockServer;
 use zero2prod::configuration::{get_configuration, DatabaseSettings};
 use zero2prod::startup::{get_connection_pool, Application};
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
@@ -20,6 +21,7 @@ static TRACING: Lazy<()> = Lazy::new(|| {
 pub struct TestApp {
     pub address: String,
     pub db_pool: PgPool,
+    pub email_server: MockServer,
 }
 
 impl TestApp {
@@ -57,10 +59,14 @@ async fn configure_database(config: &DatabaseSettings) -> PgPool {
 pub async fn spawn_app() -> TestApp {
     Lazy::force(&TRACING);
 
+    let email_server = MockServer::start().await;
+
     let configuration = {
         let mut c = get_configuration().expect("Failed to read configuration.");
         c.database.database_name = Uuid::new_v4().to_string();
         c.application.port = 0;
+        c.email_client.base_url = email_server.uri();
+
         c
     };
 
@@ -76,5 +82,6 @@ pub async fn spawn_app() -> TestApp {
         // How do we get these?
         address,
         db_pool: get_connection_pool(&configuration.database),
+        email_server,
     }
 }
