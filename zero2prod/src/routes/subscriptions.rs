f62303061dbdbf0a58bd63e75e10f3eacc929cd8
Oliver Forral <intrepion@gmail.com>
2022-01-14 02:25:04 -0700

    better error messages in log for store token errors

--- zero2prod/src/routes/subscriptions.rs
+++ zero2prod/src/routes/subscriptions.rs
@@ -14,8 +14,13 @@ pub struct FormData {
     name: String,
 }
 
-#[derive(Debug)]
 pub struct StoreTokenError(sqlx::Error);
+
+impl std::fmt::Debug for StoreTokenError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        error_chain_fmt(self, f)
+    }
+}
 
 impl std::fmt::Display for StoreTokenError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
@@ -24,6 +29,12 @@ impl std::fmt::Display for StoreTokenError {
             "A database error was encountered while \
             trying to store a subscription token."
         )
+    }
+}
+
+impl std::error::Error for StoreTokenError {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        Some(&self.0)
     }
 }
 
@@ -174,4 +185,17 @@ fn generate_subscription_token() -> String {
         .map(char::from)
         .take(25)
         .collect()
+}
+
+fn error_chain_fmt(
+    e: &impl std::error::Error,
+    f: &mut std::fmt::Formatter<'_>,
+) -> std::fmt::Result {
+    writeln!(f, "{}\n", e)?;
+    let mut current = e.source();
+    while let Some(cause) = current {
+        writeln!(f, "Caused by:\n\t{}", cause)?;
+        current = cause.source();
+    }
+    Ok(())
 }
