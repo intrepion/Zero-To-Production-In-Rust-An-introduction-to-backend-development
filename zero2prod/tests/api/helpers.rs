25f6976922797b9461afda28d2a069e232af3889
Oliver Forral <intrepion@gmail.com>
2022-01-14 00:45:12 -0700

    properly testing the subscription confirmation link

--- zero2prod/tests/api/helpers.rs
+++ zero2prod/tests/api/helpers.rs
@@ -22,6 +22,7 @@ pub struct TestApp {
     pub address: String,
     pub db_pool: PgPool,
     pub email_server: MockServer,
+    pub port: u16,
 }
 
 impl TestApp {
@@ -75,7 +76,8 @@ pub async fn spawn_app() -> TestApp {
     let application = Application::build(configuration.clone())
         .await
         .expect("Failed to build application.");
-    let address = format!("http://127.0.0.1:{}", application.port());
+    let application_port = application.port();
+    let address = format!("http://127.0.0.1:{}", application_port);
     let _ = tokio::spawn(application.run_until_stopped());
 
     TestApp {
@@ -83,5 +85,6 @@ pub async fn spawn_app() -> TestApp {
         address,
         db_pool: get_connection_pool(&configuration.database),
         email_server,
+        port: application_port,
     }
 }
