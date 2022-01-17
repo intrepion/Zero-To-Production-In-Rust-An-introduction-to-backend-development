82b231609a19e14ff6417d1e7dea363f2346cd0b
Oliver Forral <intrepion@gmail.com>
2022-01-17 04:15:20 -0700

    refactoring errors

--- scripts/watch_changes.sh
+++ scripts/watch_changes.sh
@@ -1,7 +1,5 @@
 cargo watch \
 -x fmt \
 -x check \
--x "audit --ignore RUSTSEC-2021-0131 --ignore RUSTSEC-2020-0159 --ignore RUSTSEC-2020-0071" \
--x "clippy -- -D warnings" \
 -x "test -- --nocapture" \
 -x "run | bunyan"
