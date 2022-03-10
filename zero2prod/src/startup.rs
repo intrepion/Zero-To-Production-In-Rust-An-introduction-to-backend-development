dfeed5ec1a6a23295562d8462a9de604cb65e8c1
Oliver Forral <intrepion@gmail.com>
2022-03-10 02:23:03 -0700

    4.3.3 The Facade Pattern

--- zero2prod/src/startup.rs
+++ zero2prod/src/startup.rs
@@ -1,8 +1,10 @@
 use crate::routes::{health_check, subscribe};
 use actix_web::{dev, middleware::Logger, web, web::Data, App, HttpServer};
+use env_logger::Env;
 use std::{io, net};
 
 pub fn run(listener: net::TcpListener, db_pool: sqlx::PgPool) -> Result<dev::Server, io::Error> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
     let db_pool = Data::new(db_pool);
     let server = HttpServer::new(move || {
         App::new()
