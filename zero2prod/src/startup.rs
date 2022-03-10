0e292d95717472524974169be9db5b81906a025f
Oliver Forral <intrepion@gmail.com>
2022-03-10 15:16:46 -0700

    4.5.14 Request Id

--- zero2prod/src/startup.rs
+++ zero2prod/src/startup.rs
@@ -1,12 +1,13 @@
 use crate::routes::{health_check, subscribe};
-use actix_web::{dev, middleware::Logger, web, web::Data, App, HttpServer};
+use actix_web::{dev, web, web::Data, App, HttpServer};
 use std::{io, net};
+use tracing_actix_web::TracingLogger;
 
 pub fn run(listener: net::TcpListener, db_pool: sqlx::PgPool) -> Result<dev::Server, io::Error> {
     let db_pool = Data::new(db_pool);
     let server = HttpServer::new(move || {
         App::new()
-            .wrap(Logger::default())
+            .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(db_pool.clone())
