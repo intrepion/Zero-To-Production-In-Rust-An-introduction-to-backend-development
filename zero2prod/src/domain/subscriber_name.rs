c19312ab79858e0219cbf5b8c6088cc56bf68491
Oliver Forral <intrepion@gmail.com>
2022-03-11 16:02:40 -0700

    6.12.1 Breaking The Domain Sub-Module

--- zero2prod/src/domain/subscriber_name.rs
+++ zero2prod/src/domain/subscriber_name.rs
@@ -38,11 +38,6 @@ mod tests {
 
 use unicode_segmentation::UnicodeSegmentation;
 
-pub struct NewSubscriber {
-    pub email: String,
-    pub name: SubscriberName,
-}
-
 #[derive(Debug)]
 pub struct SubscriberName(String);
 
