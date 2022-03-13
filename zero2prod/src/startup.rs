82c6ed768528e9d89a0a513e83e933a54eba161f
Oliver Forral <intrepion@gmail.com>
2022-03-13 03:44:27 -0600

    7.3.8.1 Extracting Our Startup Code

--- zero2prod/src/startup.rs
+++ zero2prod/src/startup.rs
@@ -1,11 +1,33 @@
 use crate::{
-    email_client,
+    configuration::Settings,
+    email_client::{self, EmailClient},
     routes::{health_check, subscribe},
 };
 use actix_web::{dev, web, web::Data, App, HttpServer};
-use sqlx::PgPool;
+use sqlx::{postgres::PgPoolOptions, PgPool};
 use std::{io, net};
 use tracing_actix_web::TracingLogger;
+
+pub async fn build(configuration: Settings) -> Result<dev::Server, io::Error> {
+    let connection_pool = PgPoolOptions::new()
+        .connect_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy_with(configuration.database.with_db());
+    let sender_email = configuration
+        .email_client
+        .sender()
+        .expect("Invalid sender email address.");
+    let email_client = EmailClient::new(
+        configuration.email_client.base_url,
+        sender_email,
+        configuration.email_client.authorization_token,
+    );
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
+    let listener = net::TcpListener::bind(address)?;
+    run(listener, connection_pool, email_client)
+}
 
 pub fn run(
     listener: net::TcpListener,
