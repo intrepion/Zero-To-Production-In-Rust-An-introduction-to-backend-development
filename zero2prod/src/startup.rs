d19f7c78b114798de358f3f8eba53c37996e7824
Oliver Forral <intrepion@gmail.com>
2022-03-12 22:51:10 -0700

    7.2.2.3 How To Reuse The Same reqwest::Client In actix-web

--- zero2prod/src/startup.rs
+++ zero2prod/src/startup.rs
@@ -1,16 +1,26 @@
-use crate::routes::{health_check, subscribe};
+use crate::{
+    email_client,
+    routes::{health_check, subscribe},
+};
 use actix_web::{dev, web, web::Data, App, HttpServer};
+use sqlx::PgPool;
 use std::{io, net};
 use tracing_actix_web::TracingLogger;
 
-pub fn run(listener: net::TcpListener, db_pool: sqlx::PgPool) -> Result<dev::Server, io::Error> {
+pub fn run(
+    listener: net::TcpListener,
+    db_pool: PgPool,
+    email_client: email_client::EmailClient,
+) -> Result<dev::Server, io::Error> {
     let db_pool = Data::new(db_pool);
+    let email_client = Data::new(email_client);
     let server = HttpServer::new(move || {
         App::new()
             .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(db_pool.clone())
+            .app_data(email_client.clone())
     })
     .listen(listener)?
     .run();
