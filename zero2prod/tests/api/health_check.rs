337df41c3f64c9e25381545142d8ce183133c0b3
Oliver Forral <intrepion@gmail.com>
2022-03-14 02:06:17 -0600

    7.7.5.1 Red Test

--- zero2prod/tests/api/health_check.rs
+++ zero2prod/tests/api/health_check.rs
@@ -2,10 +2,10 @@ use crate::helpers::spawn_app;
 
 #[tokio::test]
 async fn health_check_works() {
-    let test_app = spawn_app().await;
+    let app = spawn_app().await;
     let client = reqwest::Client::new();
     let response = client
-        .get(&format!("{}/health_check", &test_app.address))
+        .get(&format!("{}/health_check", &app.address))
         .send()
         .await
         .expect("Failed to execute request.");
--- zero2prod/tests/api/subscriptions.rs
+++ zero2prod/tests/api/subscriptions.rs
@@ -6,29 +6,29 @@ use wiremock::{
 
 #[tokio::test]
 async fn subscribe_returns_a_200_for_valid_form_data() {
-    let test_app = spawn_app().await;
+    let app = spawn_app().await;
 
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
 
     Mock::given(path("/email"))
         .and(method("POST"))
         .respond_with(ResponseTemplate::new(200))
-        .mount(&test_app.email_server)
+        .mount(&app.email_server)
         .await;
 
-    let response = test_app.post_subscriptions(body.into()).await;
+    let response = app.post_subscriptions(body.into()).await;
 
     assert_eq!(200, response.status().as_u16());
 }
 #[tokio::test]
 async fn subscribe_persists_the_new_subscriber() {
-    let test_app = spawn_app().await;
+    let app = spawn_app().await;
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
 
-    test_app.post_subscriptions(body.into()).await;
+    app.post_subscriptions(body.into()).await;
 
     let saved = sqlx::query!("SELECT email, name, status FROM subscriptions",)
-        .fetch_one(&test_app.db_pool)
+        .fetch_one(&app.db_pool)
         .await
         .expect("Failed to fetch saved subscription.");
 
@@ -39,7 +39,7 @@ async fn subscribe_persists_the_new_subscriber() {
 
 #[tokio::test]
 async fn subscribe_returns_a_400_when_data_is_missing() {
-    let test_app = spawn_app().await;
+    let app = spawn_app().await;
     let test_cases = vec![
         ("name=le%20guin", "missing the email"),
         ("email=ursula_le_guin%40gmail.com", "missing the name"),
@@ -47,7 +47,7 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
     ];
 
     for (invalid_body, error_message) in test_cases {
-        let response = test_app.post_subscriptions(invalid_body.into()).await;
+        let response = app.post_subscriptions(invalid_body.into()).await;
 
         assert_eq!(
             400,
@@ -60,7 +60,7 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
 
 #[tokio::test]
 async fn subscribe_returns_a_400_when_fields_are_present_but_invalid() {
-    let test_app = spawn_app().await;
+    let app = spawn_app().await;
     let test_cases = vec![
         ("name=&email=ursula_le_guin%40gmail.com", "empty name"),
         ("name=Ursula&email=", "empty email"),
@@ -68,7 +68,7 @@ async fn subscribe_returns_a_400_when_fields_are_present_but_invalid() {
     ];
 
     for (body, description) in test_cases {
-        let response = test_app.post_subscriptions(body.into()).await;
+        let response = app.post_subscriptions(body.into()).await;
 
         assert_eq!(
             400,
