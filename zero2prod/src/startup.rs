46074ba8add078a54d4dbe0aa694c9f072c63ff3
Oliver Forral <intrepion@gmail.com>
2022-03-15 23:18:15 -0600

    3.9.2 actix-web Workers

--- zero2prod/src/startup.rs
+++ zero2prod/src/startup.rs
@@ -4,11 +4,13 @@ use sqlx::PgConnection;
 use std::net::TcpListener;
 
 pub fn run(listener: TcpListener, connection: PgConnection) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
+    let connection = web::Data::new(connection);
+
+    let server = HttpServer::new(move || {
         App::new()
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
-            .app_data(connection)
+            .app_data(connection.clone())
     })
     .listen(listener)?
     .run();
