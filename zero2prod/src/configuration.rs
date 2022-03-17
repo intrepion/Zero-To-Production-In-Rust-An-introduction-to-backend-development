0bce5136147ee504c36e4d4fe0f9c32c1c0dcbab
Oliver Forral <intrepion@gmail.com>
2022-03-17 10:25:09 -0600

    5.4.3 How To Inject Secrets Using Environment Variables

--- zero2prod/src/configuration.rs
+++ zero2prod/src/configuration.rs
@@ -1,4 +1,5 @@
 use secrecy::{ExposeSecret, Secret};
+use serde_aux::field_attributes::deserialize_number_from_string;
 
 pub enum Environment {
     Local,
@@ -30,6 +31,7 @@ impl TryFrom<String> for Environment {
 
 #[derive(serde::Deserialize)]
 pub struct ApplicationSettings {
+    #[serde(deserialize_with = "deserialize_number_from_string")]
     pub port: u16,
     pub host: String,
 }
@@ -38,6 +40,7 @@ pub struct ApplicationSettings {
 pub struct DatabaseSettings {
     pub username: String,
     pub password: Secret<String>,
+    #[serde(deserialize_with = "deserialize_number_from_string")]
     pub port: u16,
     pub host: String,
     pub database_name: String,
@@ -76,16 +79,15 @@ pub fn get_configuration() -> Result<Settings, config::ConfigError> {
     let mut settings = config::Config::default();
     let base_path = std::env::current_dir().expect("Failed to determine the current directory");
     let configuration_directory = base_path.join("configuration");
-
     settings.merge(config::File::from(configuration_directory.join("base")).required(true))?;
-
     let environment: Environment = std::env::var("APP_ENVIRONMENT")
         .unwrap_or_else(|_| "local".into())
         .try_into()
         .expect("Failed to parse APP_ENVIRONMENT.");
-
     settings.merge(
         config::File::from(configuration_directory.join(environment.as_str())).required(true),
     )?;
+
+    settings.merge(config::Environment::with_prefix("app").separator("__"))?;
     settings.try_into()
 }
