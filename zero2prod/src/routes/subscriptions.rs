0a3b9c4a6452437f5a4da543a67998991123abd4
Oliver Forral <intrepion@gmail.com>
2022-03-17 20:33:34 -0600

    6.7.1 Converting parse To Return Result

--- zero2prod/src/routes/subscriptions.rs
+++ zero2prod/src/routes/subscriptions.rs
@@ -59,7 +59,7 @@ pub fn is_valid_name(s: &str) -> bool {
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
     let new_subscriber = NewSubscriber {
         email: form.0.email,
-        name: SubscriberName::parse(form.0.name),
+        name: SubscriberName::parse(form.0.name).expect("Name validation failed."),
     };
     match insert_subscriber(&pool, &new_subscriber).await {
         Ok(_) => HttpResponse::Ok().finish(),
