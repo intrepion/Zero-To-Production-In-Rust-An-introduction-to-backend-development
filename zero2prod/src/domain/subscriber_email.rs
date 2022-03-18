1db253388f59ce1425c39b1a1dade283183a1cd6
Oliver Forral <intrepion@gmail.com>
2022-03-18 02:07:28 -0600

    6.13.4 Implementing The Arbitrary Trait

--- zero2prod/src/domain/subscriber_email.rs
+++ zero2prod/src/domain/subscriber_email.rs
@@ -23,6 +23,16 @@ impl AsRef<str> for SubscriberEmail {
 mod tests {
     use super::SubscriberEmail;
     use claim::assert_err;
+    use fake::{faker::internet::en::SafeEmail, Fake};
+
+    #[derive(Debug, Clone)]
+    struct ValidEmailFixture(pub String);
+    impl quickcheck::Arbitrary for ValidEmailFixture {
+        fn arbitrary<G: quickcheck::Gen>(g: &mut G) -> Self {
+            let email = SafeEmail().fake_with_rng(g);
+            Self(email)
+        }
+    }
 
     #[test]
     fn empty_string_is_rejected() {
@@ -43,7 +53,7 @@ mod tests {
     }
 
     #[quickcheck_macros::quickcheck]
-    fn valid_emails_are_parsed_successfully(valid_email: String) -> bool {
-        SubscriberEmail::parse(valid_email).is_ok()
+    fn valid_emails_are_parsed_successfully(valid_email: ValidEmailFixture) -> bool {
+        SubscriberEmail::parse(valid_email.0).is_ok()
     }
 }
