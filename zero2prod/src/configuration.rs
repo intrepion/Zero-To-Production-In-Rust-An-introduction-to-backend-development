9a044b271fc7c0a0f1cec2a1a0584ad0b98ab6b3
Oliver Forral <intrepion@gmail.com>
2022-03-18 02:36:57 -0600

    7.2.2.4 Configuring Our EmailClient

--- zero2prod/src/configuration.rs
+++ zero2prod/src/configuration.rs
@@ -1,3 +1,4 @@
+use crate::domain::SubscriberEmail;
 use secrecy::{ExposeSecret, Secret};
 use serde_aux::field_attributes::deserialize_number_from_string;
 use sqlx::{
@@ -74,9 +75,22 @@ impl DatabaseSettings {
 }
 
 #[derive(serde::Deserialize)]
+pub struct EmailClientSettings {
+    pub base_url: String,
+    pub sender_email: String,
+}
+
+impl EmailClientSettings {
+    pub fn sender(&self) -> Result<SubscriberEmail, String> {
+        SubscriberEmail::parse(self.sender_email.clone())
+    }
+}
+
+#[derive(serde::Deserialize)]
 pub struct Settings {
     pub database: DatabaseSettings,
     pub application: ApplicationSettings,
+    pub email_client: EmailClientSettings,
 }
 
 pub fn get_configuration() -> Result<Settings, config::ConfigError> {
