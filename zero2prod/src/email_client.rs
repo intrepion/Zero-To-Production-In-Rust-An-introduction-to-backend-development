c533608a170ada0ab18463eff27ad761becc0f0e
Oliver Forral <intrepion@gmail.com>
2022-03-18 03:14:56 -0600

    7.2.5.0.2 Body

--- zero2prod/src/email_client.rs
+++ zero2prod/src/email_client.rs
@@ -52,6 +52,7 @@ impl EmailClient {
 }
 
 #[derive(serde::Serialize)]
+#[serde(rename_all = "PascalCase")]
 struct SendEmailRequest {
     from: String,
     to: String,
@@ -74,8 +75,26 @@ mod tests {
     use secrecy::Secret;
     use wiremock::{
         matchers::{header, header_exists, method, path},
-        {Mock, MockServer, ResponseTemplate},
+        {Mock, MockServer, Request, ResponseTemplate},
     };
+
+    struct SendEmailBodyMatcher;
+
+    impl wiremock::Match for SendEmailBodyMatcher {
+        fn matches(&self, request: &Request) -> bool {
+            let result: Result<serde_json::Value, _> = serde_json::from_slice(&request.body);
+            if let Ok(body) = result {
+                dbg!(&body);
+                body.get("From").is_some()
+                    && body.get("To").is_some()
+                    && body.get("Subject").is_some()
+                    && body.get("HtmlBody").is_some()
+                    && body.get("TextBody").is_some()
+            } else {
+                false
+            }
+        }
+    }
 
     #[tokio::test]
     async fn send_email_fires_a_request_to_base_url() {
@@ -90,6 +109,7 @@ mod tests {
             .and(header("Content-Type", "application/json"))
             .and(path("/email"))
             .and(method("POST"))
+            .and(SendEmailBodyMatcher)
             .respond_with(ResponseTemplate::new(200))
             .expect(1)
             .mount(&mock_server)
