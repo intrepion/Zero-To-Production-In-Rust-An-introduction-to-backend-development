264db49f85349dde502b290b25e1d375b68a6fa3
Oliver Forral <intrepion@gmail.com>
2022-03-22 17:04:45 -0600

    10.7.5.3 A Typed Interface To Session

--- zero2prod/src/routes/login/post.rs
+++ zero2prod/src/routes/login/post.rs
@@ -3,8 +3,8 @@ use crate::{
         AuthError, {validate_credentials, Credentials},
     },
     routes::error_chain_fmt,
+    session_state::TypedSession,
 };
-use actix_session::Session;
 use actix_web::{error::InternalError, http::header::LOCATION, web, HttpResponse};
 use actix_web_flash_messages::FlashMessage;
 use secrecy::Secret;
@@ -37,7 +37,7 @@ fields(username=tracing::field::Empty, user_id=tracing::field::Empty)
 pub async fn login(
     form: web::Form<FormData>,
     pool: web::Data<PgPool>,
-    session: Session,
+    session: TypedSession,
 ) -> Result<HttpResponse, InternalError<LoginError>> {
     let credentials = Credentials {
         username: form.0.username,
@@ -49,7 +49,7 @@ pub async fn login(
             tracing::Span::current().record("user_id", &tracing::field::display(&user_id));
             session.renew();
             session
-                .insert("user_id", user_id)
+                .insert_user_id(user_id)
                 .map_err(|e| login_redirect(LoginError::UnexpectedError(e.into())))?;
             Ok(HttpResponse::SeeOther()
                 .insert_header((LOCATION, "/admin/dashboard"))
