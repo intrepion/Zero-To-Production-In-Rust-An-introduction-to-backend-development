628e53cd8b9fc46bf58ac57e1f486fe0f6079439
Oliver Forral <intrepion@gmail.com>
2022-03-22 08:57:55 -0600

    10.6.4.13 How To Delete A Cookie In actix-web

--- zero2prod/src/routes/login/get.rs
+++ zero2prod/src/routes/login/get.rs
@@ -1,4 +1,8 @@
-use actix_web::{http::header::ContentType, HttpRequest, HttpResponse};
+use actix_web::{
+    cookie::{time::Duration, Cookie},
+    http::header::ContentType,
+    HttpRequest, HttpResponse,
+};
 
 pub async fn login_form(request: HttpRequest) -> HttpResponse {
     let error_html = match request.cookie("_flash") {
@@ -7,8 +11,9 @@ pub async fn login_form(request: HttpRequest) -> HttpResponse {
             format!("<p><i>{}</i></p>", cookie.value())
         }
     };
-    HttpResponse::Ok()
+    let mut response = HttpResponse::Ok()
         .content_type(ContentType::html())
+        .cookie(Cookie::build("_flash", "").max_age(Duration::ZERO).finish())
         .body(format!(
             r#"<!DOCTYPE html>
 <html lang="en">
@@ -37,5 +42,9 @@ pub async fn login_form(request: HttpRequest) -> HttpResponse {
         </form>
     </body>
 </html>"#,
-        ))
+        ));
+    response
+        .add_removal_cookie(&Cookie::new("_flash", ""))
+        .unwrap();
+    response
 }
