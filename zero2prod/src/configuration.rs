4bff461c466113b6a03ef936440359d7ec054683
Oliver Forral <intrepion@gmail.com>
2022-04-13 18:22:51 -0600

    3.8.5.2.2 - Reading A Configuration File

--- zero2prod/src/configuration.rs
+++ zero2prod/src/configuration.rs
@@ -1,1 +1,22 @@
+#[derive(serde::Deserialize)]
+pub struct DatabaseSettings {
+    pub database_name: String,
+    pub host: String,
+    pub password: String,
+    pub port: u16,
+    pub username: String,
+}
 
+#[derive(serde::Deserialize)]
+pub struct Settings {
+    pub application_port: u16,
+    pub database: DatabaseSettings,
+}
+
+pub fn get_configuration() -> Result<Settings, config::ConfigError> {
+    let mut settings = config::Config::default();
+
+    settings.merge(config::File::with_name("configuration"))?;
+
+    settings.try_into()
+}
