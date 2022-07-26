079d0885fa203104dbac3f540997dfe679f21075
Oliver Forral <intrepion@gmail.com>
2022-07-26 03:11:18 -0600

    4.5.14 Request Id

--- zero2prod/src/routes/subscriptions.rs
+++ zero2prod/src/routes/subscriptions.rs
@@ -37,10 +37,9 @@ pub async fn insert_subscriber(pool: &PgPool, form: &FormData) -> Result<(), sql
     name = "Adding a new subscriber",
     skip(form, pool),
     fields(
-    request_id = %Uuid::new_v4(),
-    subscriber_email = %form.email,
-    subscriber_name= %form.name
-)
+        subscriber_email = %form.email,
+        subscriber_name = %form.name
+    )
 )]
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
     match insert_subscriber(&pool, &form).await {
