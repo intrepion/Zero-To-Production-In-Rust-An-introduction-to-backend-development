5638c39ac4d4c900c510d96c898c94f38d6e8182
Oliver Forral <intrepion@gmail.com>
2022-07-28 22:57:59 -0600

    6.13.1 How To Generate Random Test Data With fake

--- zero2prod/src/domain/subscriber_email.rs
+++ zero2prod/src/domain/subscriber_email.rs
@@ -23,19 +23,30 @@ impl AsRef<str> for SubscriberEmail {
 mod tests {
     use super::SubscriberEmail;
     use claim::assert_err;
+    use fake::faker::internet::en::SafeEmail;
+    use fake::Fake;
+
     #[test]
     fn empty_string_is_rejected() {
         let email = "".to_string();
         assert_err!(SubscriberEmail::parse(email));
     }
+
     #[test]
     fn email_missing_at_symbol_is_rejected() {
         let email = "ursuladomain.com".to_string();
         assert_err!(SubscriberEmail::parse(email));
     }
+
     #[test]
     fn email_missing_subject_is_rejected() {
         let email = "@domain.com".to_string();
         assert_err!(SubscriberEmail::parse(email));
+    }
+
+    #[test]
+    fn valid_emails_are_parsed_successfully() {
+        let email = SafeEmail().fake();
+        claim::assert_ok!(SubscriberEmail::parse(email));
     }
 }
