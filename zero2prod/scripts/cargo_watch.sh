d826f1fbbb2e9e072afa7ff3ccb2e21906a1d296
Oliver Forral <intrepion@gmail.com>
2022-08-04 18:44:21 -0600

    7.3.8.2 Testing Hooks In Our Startup Logic

--- zero2prod/scripts/cargo_watch.sh
+++ zero2prod/scripts/cargo_watch.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-cargo watch -x fmt -x clippy -x check -x t -x "audit --deny warnings --ignore RUSTSEC-2020-0071" -x "+nightly udeps"
+cargo watch -x fmt -x clippy -x check -x t -x "audit --deny warnings --ignore RUSTSEC-2020-0071 --ignore RUSTSEC-2020-0159" -x "+nightly udeps"
--- zero2prod/src/configuration.rs
+++ zero2prod/src/configuration.rs
@@ -34,14 +34,14 @@ impl TryFrom<String> for Environment {
     }
 }
 
-#[derive(serde::Deserialize)]
+#[derive(serde::Deserialize, Clone)]
 pub struct ApplicationSettings {
     #[serde(deserialize_with = "deserialize_number_from_string")]
     pub port: u16,
     pub host: String,
 }
 
-#[derive(serde::Deserialize)]
+#[derive(serde::Deserialize, Clone)]
 pub struct DatabaseSettings {
     pub username: String,
     pub password: Secret<String>,
@@ -74,7 +74,7 @@ impl DatabaseSettings {
     }
 }
 
-#[derive(serde::Deserialize)]
+#[derive(serde::Deserialize, Clone)]
 pub struct EmailClientSettings {
     pub base_url: String,
     pub sender_email: String,
@@ -92,7 +92,7 @@ impl EmailClientSettings {
     }
 }
 
-#[derive(serde::Deserialize)]
+#[derive(serde::Deserialize, Clone)]
 pub struct Settings {
     pub database: DatabaseSettings,
     pub application: ApplicationSettings,
