d55e30f6914226d6e35b7291cdcb25022c2ebb17
Oliver Forral <intrepion@gmail.com>
2022-09-22 08:44:45 -0600

    Test Isolation

--- scripts/cargo_watch.sh
+++ scripts/cargo_watch.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-cargo watch -x fmt -x clippy -x check -x test -x audit -x "+nightly udeps"
+cargo watch -x fmt -x clippy -x check -x test -x "audit --ignore RUSTSEC-2020-0071" -x "+nightly udeps"
