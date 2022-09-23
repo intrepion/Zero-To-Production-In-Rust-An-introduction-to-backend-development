2047fdddd39d12d406faab47d7a7418e7fc999d3
Oliver Forral <intrepion@gmail.com>
2022-09-23 14:35:25 -0600

    5.4.4 Connecting To Digital Ocean’s Postgres Instance

--- src/startup.rs
+++ src/startup.rs
@@ -14,8 +14,8 @@ pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Er
         let server = HttpServer::new(move || {
             App::new()
                 .wrap(TracingLogger::default())
-                .route("/subscriptions", web::post().to(subscribe))
                 .route("/health_check", web::get().to(health_check))
+                .route("/subscriptions", web::post().to(subscribe))
                 .app_data(db_pool.clone())
         })
         .listen(listener)?
