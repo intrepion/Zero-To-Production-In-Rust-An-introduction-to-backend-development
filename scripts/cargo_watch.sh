2aa20745be500b2db3cb2f66bd96b89f3d4ef727
Oliver Forral <intrepion@gmail.com>
2022-09-22 21:15:09 -0600

    4.5.9 Removing Unused Dependencies

--- scripts/cargo_watch.sh
+++ scripts/cargo_watch.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-cargo watch -x fmt -x clippy -x check -x test -x "audit --ignore RUSTSEC-2020-0071" -x "+nightly udeps"
+cargo watch -x fmt -x clippy -x check -x test -x "audit --ignore RUSTSEC-2020-0071 --ignore RUSTSEC-2021-0139" -x "+nightly udeps"
