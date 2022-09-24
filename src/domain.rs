8fc31fff7e51a89e71403a0b15c8bd2b8b2eace2
Oliver Forral <intrepion@gmail.com>
2022-09-23 19:09:58 -0600

    6.5.1 AsRef

--- src/domain.rs
+++ src/domain.rs
@@ -8,10 +8,6 @@ pub struct NewSubscriber {
 pub struct SubscriberName(String);
 
 impl SubscriberName {
-    pub fn inner_ref(&self) -> &str {
-        &self.0
-    }
-
     pub fn parse(s: String) -> SubscriberName {
         let is_empty_or_whitespace = s.trim().is_empty();
 
@@ -24,5 +20,11 @@ impl SubscriberName {
         } else {
             Self(s)
         }
+    }
+}
+
+impl AsRef<str> for SubscriberName {
+    fn as_ref(&self) -> &str {
+        &self.0
     }
 }
