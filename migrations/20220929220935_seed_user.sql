508ed2529268e8f764d1ca4e7a740b01ae46edd2
Oliver Forral <intrepion@gmail.com>
2022-09-29 17:26:22 -0600

    10.8.1 Database Migration

---
+++ migrations/20220929220935_seed_user.sql
@@ -0,0 +1,6 @@
+INSERT INTO users (user_id, username, password_hash)
+VALUES (
+    'a47bedfd-24ff-4e26-b867-ce8eedcd10f9',
+    'admin',
+    '$argon2id$v=19$m=15000,t=2,p=1$7zj/b1/W8pPIv+lRtI+lTg$oW6So9cCXuKarS7xBSyNnPKhCo81/X9L/R2mvo7jGFI'
+);
