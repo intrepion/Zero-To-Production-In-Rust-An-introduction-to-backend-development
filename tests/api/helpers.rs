b2ba38ca392041695c3694fe52bd580bcb410853
Oliver Forral <intrepion@gmail.com>
2022-10-17 00:21:58 -0600

    9.5.1 Test Invalid Inputs

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -56,6 +56,15 @@ impl TestApp {
         ConfirmationLinks { html, plain_text }
     }
 
+    pub async fn post_newsletters(&self, body: serde_json::Value) -> reqwest::Response {
+        reqwest::Client::new()
+            .post(&format!("{}/newsletters", &self.address))
+            .json(&body)
+            .send()
+            .await
+            .expect("Failed to execute request.")
+    }
+
     pub async fn post_subscriptions(&self, body: String) -> reqwest::Response {
         reqwest::Client::new()
             .post(&format!("{}/subscriptions", &self.address))
