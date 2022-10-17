0866c5c75cd940b96dbb30fa3c97b7f88a1c681d
Oliver Forral <intrepion@gmail.com>
2022-10-17 13:00:54 -0600

    10.6.4.15 actix-web-flash-messages

--- src/startup.rs
+++ src/startup.rs
@@ -1,9 +1,11 @@
 use actix_web::{
+    cookie::Key,
     dev::Server,
     web::{self, Data},
     App, HttpServer,
 };
-use secrecy::Secret;
+use actix_web_flash_messages::{storage::CookieMessageStore, FlashMessagesFramework};
+use secrecy::{ExposeSecret, Secret};
 use sqlx::{postgres::PgPoolOptions, PgPool};
 use std::net::TcpListener;
 use tracing_actix_web::TracingLogger;
@@ -82,8 +84,12 @@ pub fn run(
     let db_pool = Data::new(db_pool);
     let email_client = Data::new(email_client);
     let base_url = Data::new(ApplicationBaseUrl(base_url));
+    let message_store =
+        CookieMessageStore::builder(Key::from(hmac_secret.0.expose_secret().as_bytes())).build();
+    let message_framework = FlashMessagesFramework::builder(message_store).build();
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(message_framework.clone())
             .wrap(TracingLogger::default())
             .route("/", web::get().to(home))
             .route("/health_check", web::get().to(health_check))
