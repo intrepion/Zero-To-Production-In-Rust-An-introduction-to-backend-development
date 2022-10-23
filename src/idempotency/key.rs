0672dc34a40fbe8d6689a7349f59116beabec722
Oliver Forral <intrepion@gmail.com>
2022-10-23 13:52:55 -0600

    11.9.1 Requirements As Tests #2

--- src/idempotency/key.rs
+++ src/idempotency/key.rs
@@ -1,5 +1,6 @@
 #[derive(Debug)]
 pub struct IdempotencyKey(String);
+
 impl TryFrom<String> for IdempotencyKey {
     type Error = anyhow::Error;
     fn try_from(s: String) -> Result<Self, Self::Error> {
