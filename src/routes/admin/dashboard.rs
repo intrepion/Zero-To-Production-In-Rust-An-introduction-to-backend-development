a85d97b6e12c29195ff424796181a8d749f6a5f3
Oliver Forral <intrepion@gmail.com>
2022-10-22 23:29:57 -0600

    Fixed spacing on HTML tidbits.

--- src/routes/admin/dashboard.rs
+++ src/routes/admin/dashboard.rs
@@ -22,22 +22,22 @@ pub async fn admin_dashboard(
         .body(format!(
             r#"<!DOCTYPE html>
 <html lang="en">
-<head>
-<meta http-equiv="content-type" content="text/html; charset=utf-8">
-<title>Admin dashboard</title>
-</head>
-<body>
-<p>Welcome {username}!</p>
-<p>Available actions:</p>
-<ol>
-<li><a href="/admin/password">Change password</a></li>
-<li>
-<form name="logoutForm" action="/admin/logout" method="post">
-<input type="submit" value="Logout">
-</form>
-</li>
-</ol>
-</body>
+    <head>
+        <meta http-equiv="content-type" content="text/html; charset=utf-8">
+        <title>Admin dashboard</title>
+    </head>
+    <body>
+        <p>Welcome {username}!</p>
+        <p>Available actions:</p>
+        <ol>
+            <li><a href="/admin/password">Change password</a></li>
+            <li>
+                <form name="logoutForm" action="/admin/logout" method="post">
+                    <input type="submit" value="Logout">
+                </form>
+            </li>
+        </ol>
+    </body>
 </html>"#,
         )))
 }
--- src/routes/login/get.rs
+++ src/routes/login/get.rs
@@ -11,31 +11,31 @@ pub async fn login_form(flash_messages: IncomingFlashMessages) -> HttpResponse {
         .content_type(ContentType::html())
         .body(format!(
             r#"<!DOCTYPE html>
-    <html lang="en">
+<html lang="en">
     <head>
-    <meta http-equiv="content-type" content="text/html; charset=utf-8">
-    <title>Login</title>
+        <meta http-equiv="content-type" content="text/html; charset=utf-8">
+        <title>Login</title>
     </head>
     <body>
-    {error_html}
-    <form action="/login" method="post">
-    <label>Username
-    <input
-    type="text"
-    placeholder="Enter Username"
-name="username"
->
-</label>
-<label>Password
-<input
-type="password"
-placeholder="Enter Password"
-name="password"
->
-</label>
-<button type="submit">Login</button>
-</form>
-</body>
+        {error_html}
+        <form action="/login" method="post">
+            <label>Username
+                <input
+                    type="text"
+                    placeholder="Enter Username"
+                    name="username"
+                >
+            </label>
+            <label>Password
+            <input
+                type="password"
+                placeholder="Enter Password"
+                name="password"
+            >
+            </label>
+            <button type="submit">Login</button>
+        </form>
+    </body>
 </html>"#,
         ))
 }
